70239cbe1c5a02a22aa519618ccadf5ce1b00283
MagicBones66 <125248651+MagicBones66@users.noreply.github.com>
2023-02-13 09:40:02 +0000

    Update streamlit_app.py
    Added Build Your Own Fruit Smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
